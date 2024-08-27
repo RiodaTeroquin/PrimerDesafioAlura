9766787dd80f2ecbe490d16807cf2ce577dcec7b
Xtra <darioquinterog5@gmail.com>
2024-08-27 00:22:54 -0500

    version de entrega

---
+++ script.js
@@ -0,0 +1,77 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";  
+    mensaje.style.backgroundImage = "none";
+
+    if (mensaje.value === "") {
+        mensaje.style.backgroundImage = "url('assets/Muñeco.png')";
+    }
+}
+
+function encriptar (stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";  
+    mensaje.style.backgroundImage = "none";
+
+    if (mensaje.value === "") {
+        mensaje.style.backgroundImage = "url('assets/Muñeco.png')";
+    }
+}
+
+function desencriptar (stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
+
+function btnCopiar() {
+    var copyText = document.getElementById("mensajeFinal")
+    if (copyText.value.trim() !=="") {
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    
+    var notificacion = document.createElement("div");
+    notificacion.textContent = "Texto copiado!";
+    notificacion.classList.add("notificacion");
+
+    document.querySelector(".notificacion-container").appendChild(notificacion);
+
+    setTimeout(function() {
+        document.querySelector(".notificacion-container").removeChild(notificacion);
+    }, 1500)
+    
+    } else {
+        var notificacion = document.createElement("div");
+    notificacion.textContent = "No hay texto para copiar!";
+    notificacion.classList.add("notificacion");
+    
+    document.querySelector(".notificacion-container").appendChild(notificacion);
+
+    setTimeout(function() {
+        document.querySelector(".notificacion-container").removeChild(notificacion);
+    }, 1500)
+    }
+}
